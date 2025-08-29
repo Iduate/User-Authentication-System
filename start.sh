211d8e3914a8f14530d70eb82b6f19b4d4447204
Iduate <davididuate11@gmail.com>
2025-08-29 20:50:36 +0100

    Add startup script to properly handle PORT environment variable

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set default port if PORT is not set
+PORT=${PORT:-8000}
+
+echo "Starting gunicorn on port $PORT"
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start gunicorn
+exec gunicorn --bind 0.0.0.0:$PORT --workers 3 --timeout 120 auth_service.wsgi:application
